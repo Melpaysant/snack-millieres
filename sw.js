17fe1b80c9145c5e7cc395f2852d12d3bae51ddf
Melpaysant <melpaysant@aol.com>
2026-06-18 18:12:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
+const CACHE = "snack-millieres-v1";
 
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE).then(cache => {
+
+      return cache.addAll([
+
+        "./",
+
+        "./index.html",
+
+        "./manifest.json"
+
+      ]);
+
+    })
+
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request).then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
+});
