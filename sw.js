9221723f391c7f9e37edc311446d9739c1cb30f4
Melpaysant <melpaysant@aol.com>
2026-06-20 09:34:38 +0200

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,37 +1,39 @@
-const CACHE = "snack-millieres-v1";
+const CACHE = "snack-millieres-v2";
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE).then(cache => {
-
       return cache.addAll([
-
         "./",
-
         "./index.html",
-
         "./manifest.json"
-
       ]);
-
     })
-
   );
 
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request).then(response => {
-
       return response || fetch(event.request);
-
     })
-
   );
-
 });
